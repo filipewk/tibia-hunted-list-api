dc2839b7517b44ed0f87a195ba9aaeeb9b02c3a1
filipewk <filipewk@gmail.com>
2020-07-27 21:28:19 -0300

    feat: ensure DbAddAccount throw if AddAccountRepository throws

--- src/data/usecases/db-add-account.spec.ts
+++ src/data/usecases/db-add-account.spec.ts
@@ -6,17 +6,17 @@ import { AddAccountRepositorySpy } from '@/data/test/mocks/db-account'
 type SutTypes = {
   sut: DbAddAccount
   hasherSpy: HasherSpy
-  addAccountRepositoryStub: AddAccountRepositorySpy
+  addAccountRepositorySpy: AddAccountRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
   const hasherSpy = new HasherSpy()
-  const addAccountRepositoryStub = new AddAccountRepositorySpy()
-  const sut = new DbAddAccount(hasherSpy, addAccountRepositoryStub)
+  const addAccountRepositorySpy = new AddAccountRepositorySpy()
+  const sut = new DbAddAccount(hasherSpy, addAccountRepositorySpy)
   return {
     sut,
     hasherSpy,
-    addAccountRepositoryStub
+    addAccountRepositorySpy
   }
 }
 
@@ -37,13 +37,21 @@ describe('DbAddAccount Usecases', () => {
   })
 
   test('Should call AddAccountRepository with correct values', async () => {
-    const { sut, addAccountRepositoryStub, hasherSpy } = makeSut()
+    const { sut, addAccountRepositorySpy, hasherSpy } = makeSut()
     const addAccountParams = mockAddAccountParams()
     await sut.add(addAccountParams)
-    expect(addAccountRepositoryStub.addAccountParams).toEqual({
+    expect(addAccountRepositorySpy.addAccountParams).toEqual({
       name: addAccountParams.name,
       email: addAccountParams.email,
       password: hasherSpy.digest
     })
+  })
+
+  test('Should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositorySpy } = makeSut()
+    jest.spyOn(addAccountRepositorySpy, 'add').mockRejectedValueOnce(new Error())
+    const addAccountParams = mockAddAccountParams()
+    const promise = sut.add(addAccountParams)
+    await expect(promise).rejects.toThrow()
   })
 })
