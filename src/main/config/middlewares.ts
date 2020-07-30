608f522ad803cccd8b88f568fe0fe605cefc5cac
filipewk <filipewk@gmail.com>
2020-07-29 21:49:27 -0300

    refactor: group middleware interfaces in a single file

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,5 +1,4 @@
-import { bodyParser } from '../middlewares/body-parser'
-import { cors } from '../middlewares/cors'
+import { bodyParser, cors } from '../middlewares'
 import { Express } from 'express'
 
 export default (app: Express): void => {
