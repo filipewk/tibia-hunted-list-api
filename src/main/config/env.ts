82ec5afd7ca813dbca8863b10295af0cf376cfcc
filipewk <filipewk@gmail.com>
2020-08-03 23:31:11 -0300

    fix: change ip docker

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  postgresUrl: process.env.DATABASE_URL || 'postgres://postgres:filipe50@172.25.0.3:5432/tibia-hunted-list',
+  postgresUrl: process.env.DATABASE_URL || 'postgres://postgres:filipe50@172.28.0.2:5432/tibia-hunted-list',
   port: process.env.PORT || 5050
 }
