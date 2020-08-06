039ba7aa2dd23dee85d814787f636839f52cb7b0
filipewk <filipewk@gmail.com>
2020-08-05 22:57:58 -0300

    feat: add adapter to express

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   secret: ')(*&^%$%^&*(bsalt)KjdjnUyReqfd%',
-  postgresUrl: process.env.DATABASE_URL || 'postgres://postgres:filipe50@172.28.0.2:5432/postgres',
+  postgresUrl: process.env.DATABASE_URL || 'postgres://postgres:filipe50@192.168.32.2:5432/api-tibia',
   port: process.env.PORT || 5050
 }
