0c4b2ab3a0a714b3b2a8510e7944c88b1ae0537b
Filipe <filipewk@gmail.com>
2020-10-06 20:49:17 -0300

    feat: ensure RemoveCharacterController return 500 if RemoveCharacter throws

--- src/presentation/controllers/character/delete-character/delete-character-controller.spec.ts
+++ src/presentation/controllers/character/delete-character/delete-character-controller.spec.ts
@@ -1,7 +1,14 @@
-import { noContent } from '@/presentation/helpers/http/http-helper'
+import { ServerError } from '@/presentation/errors'
+import { noContent, serverError } from '@/presentation/helpers/http/http-helper'
 import { RemoveCharacterSpy } from '@/presentation/test/mocks/character'
 import { HttpRequest } from '../../login/login/login-controller-protocols'
 import { RemoveCharacterController } from './delete-character-controller'
+
+const mockRequest = (): HttpRequest => ({
+  params: {
+    characterId: 'any_id'
+  }
+})
 
 type SutTypes = {
   sut: RemoveCharacterController
@@ -17,25 +24,24 @@ const makeSut = (): SutTypes => {
   }
 }
 
-const mockRequest = (): HttpRequest => ({
-  params: {
-    characterId: 'any_id'
-  }
-})
-
 describe('RemoveCharacter Controller', () => {
   test('Should call RemoveCharacter', async () => {
     const { sut, removeCharacterStub } = makeSut()
     const removeSpy = jest.spyOn(removeCharacterStub, 'remove')
-    const httpRequest = mockRequest()
-    await sut.handle(httpRequest)
+    await sut.handle(mockRequest())
     expect(removeSpy).toHaveBeenCalled()
   })
 
   test('Should return 204 if deleted character succeed', async () => {
     const { sut } = makeSut()
-    const httpRequest = mockRequest()
-    const httpResponse = await sut.handle(httpRequest)
+    const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(noContent())
+  })
+
+  test('Should return 500 if RemoveCharacter throw', async () => {
+    const { sut, removeCharacterStub } = makeSut()
+    jest.spyOn(removeCharacterStub, 'remove').mockRejectedValueOnce(new Error())
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverError(new ServerError(null)))
   })
 })
--- src/presentation/controllers/character/delete-character/delete-character-controller.ts
+++ src/presentation/controllers/character/delete-character/delete-character-controller.ts
@@ -1,5 +1,6 @@
 import { RemoveCharacter } from '@/domain/usecases/character/delete-character'
-import { noContent } from '@/presentation/helpers/http/http-helper'
+import { ServerError } from '@/presentation/errors'
+import { noContent, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../login/login/login-controller-protocols'
 
 export class RemoveCharacterController implements Controller {
@@ -8,8 +9,12 @@ export class RemoveCharacterController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { characterId } = httpRequest.params
-    await this.removeCharacter.remove(characterId)
-    return noContent()
+    try {
+      const { characterId } = httpRequest.params
+      await this.removeCharacter.remove(characterId)
+      return noContent()
+    } catch (error) {
+      return serverError(new ServerError(error.stack))
+    }
   }
 }
