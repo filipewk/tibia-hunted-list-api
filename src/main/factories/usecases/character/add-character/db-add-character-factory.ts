8843bcec62ae17527e7f187a595c36bd1a1b2c6f
Filipe <filipewk@gmail.com>
2020-10-05 21:13:08 -0300

    refactor: move db-add-character to own folder

--- src/main/factories/usecases/character/add-character/db-add-character-factory.ts
+++ src/main/factories/usecases/character/add-character/db-add-character-factory.ts
@@ -1,6 +1,6 @@
 import { AddCharacter } from '@/domain/usecases/character/add-character'
 import { CharacterPostgresRepository } from '@/infra/db/postgres/character-repository/character-repository'
-import { DbAddCharacter } from '@/data/usecases/character/db-add-character'
+import { DbAddCharacter } from '@/data/usecases/character/add-character/db-add-character'
 
 export const makeDbAddCharacter = (): AddCharacter => {
   const characterPostegresRepository = new CharacterPostgresRepository()
