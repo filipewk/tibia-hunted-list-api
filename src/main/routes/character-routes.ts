acb17bf2eea74b68b9eb6100da96b6cb77589ab1
Filipe <filipewk@gmail.com>
2020-10-06 22:08:23 -0300

    feat: create route delete

--- src/main/routes/character-routes.ts
+++ src/main/routes/character-routes.ts
@@ -1,9 +1,11 @@
-import { adaptRoute } from '@/main/adapters/express-route-adapter'
 import { makeAddCharacterController } from '../factories/controllers/character/add-character/add-character-controller-factory'
-import { Router } from 'express'
 import { makeLoadCharactersController } from '../factories/controllers/character/load-characters/load-characters-controller-factory'
+import { makeDeleteCharacterController } from '../factories/controllers/character/delete-character/delete-characters-controller-factory'
+import { adaptRoute } from '@/main/adapters/express-route-adapter'
+import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/characters', adaptRoute(makeAddCharacterController()))
   router.get('/characters', adaptRoute(makeLoadCharactersController()))
+  router.delete('/character/:characterId', adaptRoute(makeDeleteCharacterController()))
 }
