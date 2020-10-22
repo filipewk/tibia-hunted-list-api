dfbcb7117c1a27fc9e93642994dd8f1696efba03
Filipe <filipewk@gmail.com>
2020-10-21 21:40:02 -0300

    refactor: create a index file for makeControllers

---
+++ src/main/factories/controllers/character/index.ts
@@ -0,0 +1,4 @@
+export * from '../character/add-character/add-character-controller-factory'
+export * from '../character/delete-character/delete-characters-controller-factory'
+export * from '../character/load-characters/load-characters-controller-factory'
+export * from '../character/update-character/update-character-controller-factory'
--- src/main/routes/character-routes.ts
+++ src/main/routes/character-routes.ts
@@ -1,11 +1,15 @@
-import { makeAddCharacterController } from '../factories/controllers/character/add-character/add-character-controller-factory'
-import { makeLoadCharactersController } from '../factories/controllers/character/load-characters/load-characters-controller-factory'
-import { makeDeleteCharacterController } from '../factories/controllers/character/delete-character/delete-characters-controller-factory'
+import {
+  makeAddCharacterController,
+  makeLoadCharactersController,
+  makeDeleteCharacterController,
+  makeUpdateCharacterController
+} from '../factories/controllers/character'
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.post('/characters', adaptRoute(makeAddCharacterController()))
   router.get('/characters', adaptRoute(makeLoadCharactersController()))
+  router.post('/characters', adaptRoute(makeAddCharacterController()))
+  router.put('/character/:characterId', adaptRoute(makeUpdateCharacterController()))
   router.delete('/character/:characterId', adaptRoute(makeDeleteCharacterController()))
 }
